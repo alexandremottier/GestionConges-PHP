34234fd5fc954d60e3d832b399b7533627544a48
Alexandre MOTTIER <am@am-networks.fr>
2023-01-15 22:27:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <body>
 <h1>Gestion des congés - Affichage</h1>
 <h2>Année en cours : <?php echo date('Y'); ?></h2>
-<button onclick="window.location.href='saisie.php'">Saisir des congés</button>
+<button onclick="window.location.href='saisie.php'" class="button">Saisir des congés</button>
 <br><br>
 <?php
 $sql = "SELECT * FROM $table";
@@ -42,7 +42,6 @@ if ($result->num_rows > 0) {
     $findate = str_replace('-"', '/', $row["datefin"]);
     $datefin = date("d/m/Y", strtotime($findate));
     $nombrejours = $row["nbjours"];
-    $link = 'class/class.remove.php?datedebut=" . $row["datedebut"] . "&datefin=" . $row["datefin"]. "'
     # echo "<tr><td> " . $datedebut. " </td><td> " . $datefin. " </td><td> " . $nombrejours . " jour(s) </td><td> " . $row["type"] . " </td><td><button onclick="window.location.href='class/class.remove.php?datedebut=" . $row["datedebut"] . "&datefin=" . $row["datefin"]. "'">Supprimer la ligne</button></td></tr>";
 
   }
@@ -54,8 +53,8 @@ $conn->close();
 </tbody>
 </table>
 <br>
-<button onclick="window.location.href='class/class.clearcp.php'">Supprimer les congés payés</button>
-<button onclick="window.location.href='class/class.clearrtt.php'">Supprimer les RTT</button>
+<button onclick="window.location.href='class/class.clearcp.php'" class="button">Supprimer les congés payés</button>
+<button onclick="window.location.href='class/class.clearrtt.php'" class="button">Supprimer les RTT</button>
 <br><br>
 <?php require('solde.php') ?>
 </body>
