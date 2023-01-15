30b3cf0e6256819ff4122f5e7b0eeeb6eca2dcff
Alexandre MOTTIER <am@am-networks.fr>
2023-01-15 22:26:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ if ($result->num_rows > 0) {
     $datefin = date("d/m/Y", strtotime($findate));
     $nombrejours = $row["nbjours"];
     $link = 'class/class.remove.php?datedebut=" . $row["datedebut"] . "&datefin=" . $row["datefin"]. "'
-    echo "<tr><td> " . $datedebut. " </td><td> " . $datefin. " </td><td> " . $nombrejours . " jour(s) </td><td> " . $row["type"] . " </td><td><button onclick="window.location.href='class/class.remove.php?datedebut=" . $row["datedebut"] . "&datefin=" . $row["datefin"]. "'">Supprimer la ligne</button></td></tr>";
+    # echo "<tr><td> " . $datedebut. " </td><td> " . $datefin. " </td><td> " . $nombrejours . " jour(s) </td><td> " . $row["type"] . " </td><td><button onclick="window.location.href='class/class.remove.php?datedebut=" . $row["datedebut"] . "&datefin=" . $row["datefin"]. "'">Supprimer la ligne</button></td></tr>";
 
   }
 } else {
